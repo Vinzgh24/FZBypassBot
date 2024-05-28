bd7b593db3de5669b9696952c3b3b79cd53b13c4
Vinzgh24 <128291675+Vinzgh24@users.noreply.github.com>
2024-05-28 18:23:20 +0700

    Update update.py

--- update.py
+++ update.py
@@ -21,30 +21,68 @@ basicConfig(
 )
 load_dotenv("config.env", override=True)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SilentDemonSD/FZBypassBot")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+if not os.path.exists("config.env"):
+    CONFIG_FILE_URL = os.environ.get("CONFIG_FILE_URL", "")
+    if len(CONFIG_FILE_URL) != 0:
+        LOGGER.info("CONFIG_FILE_URL is found! Downloading CONFIG_FILE_URL...")
+        r = requests.get(
+            CONFIG_FILE_URL
+        )
+        
+        if not r.ok:
+            LOGGER.error(f"Failed to download config.env! ERROR: [{r.status_code}] {r.text}")
+    
+        with open("config.env", "wb+") as file:
+            file.write(r.content)
+    else:
+        LOGGER.warning("CONFIG_FILE_URL is not found! Using local config.env instead...")
+            
+load_dotenv("config.env", override=True)
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+if len(BOT_TOKEN) == 0:
+    LOGGER.error("BOT_TOKEN is not found!")
+    exit(1)
+
+bot_id = BOT_TOKEN.split(":", 1)[0]
+
+UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", None)
+if (
+    UPSTREAM_REPO is not None
+    and UPSTREAM_REPO.startswith("#")
+):
+    UPSTREAM_REPO = None
+
+UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", None)
+if UPSTREAM_BRANCH is None:
+    UPSTREAM_BRANCH = "master"
+    
 if UPSTREAM_REPO is not None:
-    if opath.exists(".git"):
-        srun(["rm", "-rf", ".git"])
+    if os.path.exists(".git"):
+        subprocess.run(
+            [
+                "rm -rf .git"
+            ], shell=True
+        )
 
-    update = srun(
+    process = subprocess.run(
         [
             f"git init -q \
-                     && git config --global user.email drxxstrange@gmail.com \
-                     && git config --global user.name SilentDemonSD \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"
-        ],
-        shell=True,
+            && git config --global user.email ryuzakinear108@gmail.com \
+            && git config --global user.name vinzgh24 \
+            && git add . \
+            && git commit -sm update -q \
+            && git remote add origin {UPSTREAM_REPO} \
+            && git fetch origin -q \
+            && git reset --hard origin/{UPSTREAM_BRANCH} -q"
+        ], shell=True
     )
 
-    if update.returncode == 0:
-        log_info("Successfully updated with latest commit from UPSTREAM_REPO")
+    if process.returncode == 0:
+        LOGGER.info("Successfully updated with latest commit from UPSTREAM_REPO!")
     else:
-        log_error(
-            "Something went wrong while updating, check UPSTREAM_REPO if valid or not!"
-        )
+        LOGGER.error(
+            "Something wrong while updating! Check UPSTREAM_REPO if valid or not!")
+
+else:
+    LOGGER.warning("UPSTREAM_REPO is not found!")
